945c10d34472651450ebc3ce5d939dcd73e8d55c
Paddy Carver <paddy@impractical.co>
2025-09-17 22:37:05 -0700

    Add example of custom funcmaps.

--- example_funcmap_test.go
+++ example_funcmap_test.go
@@ -2,13 +2,15 @@ package temple_test
 
 import (
 	"context"
+	"html/template"
 	"log/slog"
 	"os"
+	"strings"
 
 	"impractical.co/temple"
 )
 
-type BasicSite struct {
+type FuncMapSite struct {
 	// anonymously embedding a *CachedSite makes MySite a Site implementation
 	*temple.CachedSite
 
@@ -16,45 +18,61 @@ type BasicSite struct {
 	Title string
 }
 
-type BasicHomePage struct {
+type FuncMapHomePage struct {
 	Name   string
-	Layout BasicLayout
+	Fruits []string
+	Layout FuncMapLayout
 }
 
-func (BasicHomePage) Templates(_ context.Context) []string {
+func (FuncMapHomePage) Templates(_ context.Context) []string {
 	return []string{"home.html.tmpl"}
 }
 
-func (page BasicHomePage) UseComponents(_ context.Context) []temple.Component {
+func (page FuncMapHomePage) UseComponents(_ context.Context) []temple.Component {
 	return []temple.Component{
 		page.Layout,
 	}
 }
 
-func (BasicHomePage) Key(_ context.Context) string {
+func (FuncMapHomePage) Key(_ context.Context) string {
 	return "home.html.tmpl"
 }
 
-func (page BasicHomePage) ExecutedTemplate(_ context.Context) string {
+func (page FuncMapHomePage) ExecutedTemplate(_ context.Context) string {
 	return page.Layout.BaseTemplate()
 }
 
-type BasicLayout struct {
+func (FuncMapHomePage) FuncMap(_ context.Context) template.FuncMap {
+	return template.FuncMap{
+		"humanize": func(input []string) string {
+			if len(input) < 1 {
+				return ""
+			}
+			if len(input) < 2 {
+				return strings.Join(input, " and ")
+			}
+			input[len(input)-1] = "and " + input[len(input)-1]
+			return strings.Join(input, ", ")
+		},
+	}
 }
 
-func (layout BasicLayout) Templates(_ context.Context) []string {
+type FuncMapLayout struct {
+}
+
+func (layout FuncMapLayout) Templates(_ context.Context) []string {
 	return []string{layout.BaseTemplate()}
 }
 
-func (BasicLayout) BaseTemplate() string {
+func (FuncMapLayout) BaseTemplate() string {
 	return "base.html.tmpl"
 }
 
-func ExampleRender_basic() {
+func ExampleRender_funcMaps() {
 	// normally you'd use something like embed.FS or os.DirFS for this
 	// for example purposes, we're just hardcoding values
 	var templates = staticFS{
-		"home.html.tmpl": `{{ define "body" }}Hello, {{ .Page.Name }}. This is my home page.{{ end }}`,
+		"home.html.tmpl": `{{ define "body" }}Hello, {{ .Page.Name }}. This is my home page. I like {{ humanize .Page.Fruits }}.{{ end }}`,
 		"base.html.tmpl": `
 <!doctype html>
 <html lang="en">
@@ -73,13 +91,14 @@ func ExampleRender_basic() {
 	// usually the context comes from the request, but here we're building it from scratch and adding a logger
 	ctx := temple.LoggingContext(context.Background(), slog.Default())
 
-	site := BasicSite{
+	site := FuncMapSite{
 		CachedSite: temple.NewCachedSite(templates),
 		Title:      "My Example Site",
 	}
-	page := BasicHomePage{
+	page := FuncMapHomePage{
 		Name:   "Visitor",
-		Layout: BasicLayout{},
+		Fruits: []string{"apples", "bananas", "oranges"},
+		Layout: FuncMapLayout{},
 	}
 	temple.Render(ctx, os.Stdout, site, page)
 
@@ -89,6 +108,6 @@ func ExampleRender_basic() {
 	// 	<head>
 	// 		<title>My Example Site</title></head>
 	// 	<body>
-	// 		Hello, Visitor. This is my home page.</body>
+	// 		Hello, Visitor. This is my home page. I like apples, bananas, and oranges.</body>
 	// </html>
 }
